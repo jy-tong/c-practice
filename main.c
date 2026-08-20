5442ead1ea504f64087e5fdc624353d43436e6a8
jy-tong <tong546474tong@163.com>
2026-08-20 20:26:17 +0800

    变量和数据类型

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello world\n");
+    int age = 18;
+	float score = 92.5;
+	char grade = 'A';
+	double pi = 3.1415926;
+
+	printf("Age:%d\n",age);
+	printf("Score: %f\n",score);
+	printf("Grade:%c\n",grade);
+	printf("Pi:%.2lf\n", pi);
+
 	return 0;
+
 }
